470dad323cf81cfb49cbd7b7c655dbb0983b4bde
zomeru <zomerzxc.19@gmail.com>
2023-02-16 21:19:56 +0800

    Setup project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import { Button } from "ant-design-vue";
+</script>
+
+<template>
+  <main>
+    <Button type="primary">Button</Button>
+  </main>
+</template>
+
+<style scoped></style>
